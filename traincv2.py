cf9aba09668c7f15c941d8ff7004a026b26bcc3b
xinglibao <xinglibao383@gmail.com>
2025-06-29 17:35:06 +0800

    0629

--- traincv2.py
+++ traincv2.py
@@ -9,14 +9,16 @@ from models.mycnetv2 import IMUPose as IMUPosev2
 
 
 if __name__ == "__main__":
-    hidden_dim, num_layers, dropout, window_size, stride, batch_size, mask_ratio, lr, weight_decay, num_epochs = 128, 2, 0.3, 250, 25, 512, 0.15, 1e-3, 1e-4, 800
-    alpha, beta, gamma = 100, 0.001, 1
+    """
+    Params: hidden_dim = 128, num_layers = 2, dropout = 0.3, window_size = 250, stride = 25, batch_size = 512, mask_ratio = 0.15 lr = 0.001, weight_decay = 0.0001, num_epochs = 800 alpha = 10, beta = 0.001, gamma = 1 
+    """
+    hidden_dim, num_layers, dropout, window_size, stride, batch_size, mask_ratio, lr, weight_decay, num_epochs, alpha, beta, gamma = 128, 2, 0.3, 250, 25, 512, 0.15, 1e-3, 1e-4, 800, 10, 0.001, 1
     model = IMUPosev2(hidden_dim=hidden_dim, num_layers=num_layers, len_output=window_size/2/50, dropout=dropout)
     train_loader, val_loader = get_dataloaders("/data/xinglibao/data/future/imu", window_size, stride, batch_size, 0.8)
     devices = [torch.device('cuda:0'), torch.device('cuda:1'), torch.device('cuda:2'), torch.device('cuda:3')]
     output_save_path = os.path.join('/home/xinglibao/workspace/future/outputs', datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
     logger = Logger(save_path=output_save_path)
-    logger.record(['使用的train2'])
+    logger.record(['使用的train2，想要复现之前最好的效果'])
     logger.record([f'Params: hidden_dim = {hidden_dim}, num_layers = {num_layers}, dropout = {dropout}, '
                 f'window_size = {window_size}, stride = {stride}, batch_size = {batch_size}, mask_ratio = {mask_ratio} '
                 f'lr = {lr}, weight_decay = {weight_decay}, num_epochs = {num_epochs} '

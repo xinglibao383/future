ddce1c9fc0b1d3483539aca82a8b14fef05d99b1
liming <xinglibao383@gmail.com>
2025-10-21 23:22:25 +0800

    see wifi data shape

--- tmp/temp5.py
+++ tmp/temp5.py
@@ -1,22 +1,62 @@
-import numpy as np
+import h5py
 
-# 1. 指定你的 .npy 文件路径
-file_path = "/home/xinglibao/workspace/future/mydata/imu/45_15_15_15/0_3_4_1080.npy"  # 替换为你的文件名或路径
+# /home/yh/.conda/envs/myfuture/bin/python /mnt/mydata/yh/liming/workspace/future/tmp/temp5.py
+if __name__ == "__main__":
+    print("================================================================================")
+    file_path = '/mnt/mydata/yh/liming/data/xrfv2/wifi/10_2_7.h5'
+    with h5py.File(file_path, 'r') as f:
+        print("Keys in the file:", list(f.keys()))
 
-# 2. 读取 .npy 文件
-data = np.load(file_path)
+        print(f"Shape of dataset {'amp'}: {f['amp'].shape}")
+        print(f"Shape of dataset {'pha'}: {f['pha'].shape}")
+        print(f"Shape of dataset {'label'}: {f['label'].shape}")
+    print("================================================================================")
+    file_path = '/mnt/mydata/yh/liming/data/xrfv2/wifi/13_2_13.h5'
+    with h5py.File(file_path, 'r') as f:
+        print("Keys in the file:", list(f.keys()))
 
-# 3. 打印数据的 shape
-print("数据 shape:", data.shape)
+        print(f"Shape of dataset {'amp'}: {f['amp'].shape}")
+        print(f"Shape of dataset {'pha'}: {f['pha'].shape}")
+        print(f"Shape of dataset {'label'}: {f['label'].shape}")
+    print("================================================================================")
+    file_path = '/mnt/mydata/yh/liming/data/xrfv2/imu/10_2_7.h5'
+    with h5py.File(file_path, 'r') as f:
+        print("Keys in the file:", list(f.keys()))
+
+        print(f"Shape of dataset {'data'}: {f['data'].shape}")
+        print(f"Dataset {'duration'}: {f['duration'][()]}")
+        print(f"Shape of dataset {'label'}: {f['label'].shape}")
+    print("================================================================================")
+    file_path = '/mnt/mydata/yh/liming/data/xrfv2/imu/13_2_13.h5'
+    with h5py.File(file_path, 'r') as f:
+        print("Keys in the file:", list(f.keys()))
+
+        print(f"Shape of dataset {'data'}: {f['data'].shape}")
+        print(f"Dataset {'duration'}: {f['duration'][()]}")
+        print(f"Shape of dataset {'label'}: {f['label'].shape}")
+    print("================================================================================")
 
 
-
-
-# 1. 指定你的 .npy 文件路径
-file_path = "/home/xinglibao/workspace/future/mydata/pose/45_15_15_15/8_2_4_30.npy"  # 替换为你的文件名或路径
-
-# 2. 读取 .npy 文件
-data = np.load(file_path)
-
-# 3. 打印数据的 shape
-print("数据 shape:", data.shape)
+"""
+    ================================================================================
+    Keys in the file: ['amp', 'label', 'pha']
+    Shape of dataset amp: (4250, 3, 3, 30)
+    Shape of dataset pha: (4250, 3, 3, 30)
+    Shape of dataset label: (10, 4)
+    ================================================================================
+    Keys in the file: ['amp', 'label', 'pha']
+    Shape of dataset amp: (3500, 3, 3, 30)
+    Shape of dataset pha: (3500, 3, 3, 30)
+    Shape of dataset label: (10, 4)
+    ================================================================================
+    Keys in the file: ['data', 'duration', 'label']
+    Shape of dataset data: (5, 4250, 6)
+    Dataset duration: 85
+    Shape of dataset label: (10, 4)
+    ================================================================================
+    Keys in the file: ['data', 'duration', 'label']
+    Shape of dataset data: (5, 3500, 6)
+    Dataset duration: 70
+    Shape of dataset label: (10, 4)
+    ================================================================================
+"""

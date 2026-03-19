2cd795996cde7c3ac8c281fe1d0e13ca4860810b
liming <xinglibao383@gmail.com>
2026-03-19 20:59:37 +0800

    清除没有用的代码

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,27 +1,6 @@
 from torch.utils.data import DataLoader, random_split
-from utils.xrf55 import XRF55
-from utils.xrfv2 import *
 from utils.xrfv22 import *
 from utils.xrfv22_max_predict_len import *
-from utils.xrfv22_select_best_solution import *
-
-
-def get_dataloaders(root_path, len_input, len_predict, batch_size, train_ratio):
-    dataset= XRF55(root_path, len_input, len_predict)
-    train_size = int(train_ratio * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-    return (DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, prefetch_factor=16, persistent_workers=True), 
-            DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, prefetch_factor=16, persistent_workers=True))
-
-
-def get_dataloaders_v2(root_path, use_len, compute_len, predict_len, stride_len, batch_size, train_ratio):
-    dataset= XRFV2(root_path, use_len, compute_len, predict_len, stride_len)
-    train_size = int(train_ratio * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-    return (DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, prefetch_factor=16, persistent_workers=True), 
-            DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, prefetch_factor=16, persistent_workers=True))
 
 
 def get_dataloaders_v3(root_path, use_len, compute_len, predict_len, stride_len, batch_size, train_ratio, exclude_device_idx=None, cross=None, cross_idx=None, mode=None, random_seed=None):
@@ -51,16 +30,5 @@ def get_dataloaders_v3_max_predict_len(root_path, use_len, compute_len, predict_
             DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, prefetch_factor=16, persistent_workers=True))
 
 
-def get_dataloaders_v3_select_best_solution(root_path, use_len, compute_len, predict_len, stride_len, batch_size, train_ratio):
-    dataset= XRFV22SelectBestSolution(root_path, use_len, compute_len, predict_len, stride_len)
-    train_size = int(train_ratio * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-    return (DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, prefetch_factor=16, persistent_workers=True), 
-            DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, prefetch_factor=16, persistent_workers=True))
-
-
 if __name__ == "__main__":
-    train_dataloader, val_dataloader = get_dataloaders("/home/xinglibao/workspace/future/data", 60, 15, 32, 0.8)
-    for i, (x, y) in enumerate(train_dataloader):
-        print(i, x.shape, y.shape)
+    pass

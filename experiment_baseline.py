6a79a3f7d121ffed8bd90ba0d477cb996b63ddcf
liming <xinglibao383@gmail.com>
2026-03-20 13:48:57 +0800

    基线模型对比

--- experiment_baseline.py
+++ experiment_baseline.py
@@ -4,6 +4,8 @@ from utils.logger import Logger
 from utils.dataloader import *
 from utils.train_baseline import train
 from models.comparison.baselines import build_baseline_model
+from utils.train3 import train as train3
+from models.posenet import *
 
 
 torch.manual_seed(3407)
@@ -11,6 +13,29 @@ torch.manual_seed(3407)
 devices = [torch.device('cuda:3')]
 output_save_path = '/mnt/mydata/yh/liming/workspace/future/outputs/experiment/baseline'
 data_root_path = '/mnt/mydata/yh/liming/workspace/future/mydata'
+
+
+def ours():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    logger = Logger(save_path=output_save_path, timestamp=timestamp)
+    logger.record([f'备注: 基线模型对比实验, baseline=aipose'])
+    mask_ratio, batch_size, lr, num_epochs, loss_func = 0.25, 256, 1e-3, 200, "l1"
+    resnet_verson, imu_generator = "resnet18", "transformer"
+    transformer_hidden, transformer_layers, transformer_nhead, transformer_dropout = 128, 2, 4, 0.1
+    use_len, compute_len, predict_len, stride_len = 60, 15, 15, 15
+    need_normalize, alpha, beta, gamma = True, 1, 1, 1
+    params = {
+        "mask_ratio": mask_ratio, "batch_size": batch_size, "lr": lr, "epochs": num_epochs, "loss_func": loss_func,
+        "resnet_verson": resnet_verson, "imu_generator": imu_generator, 
+        "transformer_hidden": transformer_hidden, "transformer_layers": transformer_layers, "transformer_nhead": transformer_nhead, "transformer_dropout": transformer_dropout, 
+        "use_len": use_len, "compute_len": compute_len, "predict_len": predict_len, "stride_len": stride_len,
+        "need_normalize": need_normalize, "alpha": alpha, "beta": beta, "gamma": gamma,
+    }
+    logger.record([", ".join([f"{k}={v}" for k, v in params.items()])])
+    imu_generator_params = (transformer_hidden, transformer_layers, transformer_nhead, transformer_dropout)
+    model = PoseNet(input_channels=30, resnet_verson=resnet_verson, imu_generator=imu_generator, imu_generator_params=imu_generator_params, target_time=int(predict_len / 15 * 50), target_poses=predict_len, num_poses=compute_len, num_keypoints=25, output_dim=2)
+    train_loader, val_loader = get_dataloaders_v3(data_root_path, use_len, compute_len, predict_len, stride_len, batch_size, 0.8, random_seed=3407)
+    train3(model, train_loader, val_loader, loss_func, mask_ratio, lr, need_normalize, alpha, beta, gamma, num_epochs, devices, output_save_path, logger, timestamp)
 
 
 def experiment_baseline(baseline="aipose"):
@@ -34,13 +59,15 @@ def experiment_baseline(baseline="aipose"):
     train_loader, val_loader = get_dataloaders_v3(data_root_path, use_len, compute_len, predict_len, stride_len, batch_size, train_ratio, random_seed=3407)
     train(model, train_loader, val_loader, loss_func, mask_ratio, lr, need_normalize, num_epochs, devices, output_save_path, logger, timestamp)
 
+
 # nohup /home/yh/.conda/envs/myfuture/bin/python /mnt/mydata/yh/liming/workspace/future/experiment_baseline.py > /dev/null 2>&1 &
 # /home/yh/.conda/envs/myfuture/bin/python /mnt/mydata/yh/liming/workspace/future/experiment_baseline.py
 if __name__ == "__main__":
-    experiment_baseline(baseline="aipose")
-    experiment_baseline(baseline="pip_like_recon")
-    experiment_baseline(baseline="tip_like_recon")
-    experiment_baseline(baseline="imuposer_like_recon")
-    experiment_baseline(baseline="dynaip_like_recon")
-    experiment_baseline(baseline="asip_like_recon")
-    experiment_baseline(baseline="mobileposer_like_recon")
+    ours()
+    # experiment_baseline(baseline="aipose")
+    # experiment_baseline(baseline="pip_like_recon")
+    # experiment_baseline(baseline="tip_like_recon")
+    # experiment_baseline(baseline="dynaip_like_recon")
+    # experiment_baseline(baseline="asip_like_recon")
+    # experiment_baseline(baseline="mobileposer_like_recon")
+    # experiment_baseline(baseline="imuposer_like_recon")
--- utils/train3.py
+++ utils/train3.py
@@ -15,43 +15,43 @@ def normalize(x, eps=1e-6):
     return (x - mean) / std
 
 
-def plot_poses(data, output_save_path, timestamp):
-    def plot_pose(pose, save_filepath):
-        x, y = pose[:,0].numpy(), pose[:,1].numpy()
-        plt.figure(figsize=(8,10))
-        plt.scatter(x, y, c='red', s=50)
-        for i,(xi, yi) in enumerate(zip(x, y)):
-            plt.text(xi+0.02, yi+0.02, f"({xi:.2f},{yi:.2f})", fontsize=8, color='blue')
-        skeleton = [
-            (0,1),(1,2),(2,3),(3,4),(1,5),(5,6),(6,7),
-            (1,8),(8,9),(9,10),(10,11),(8,12),(12,13),(13,14),
-            (0,15),(15,17),(0,16),(16,18),(14,19),(19,20),(14,21),
-            (11,22),(22,23),(11,24)
-        ]
-        for i,j in skeleton:
-            plt.plot([x[i],x[j]], [y[i],y[j]], 'g-', linewidth=2)
-        plt.gca().invert_yaxis()
-        plt.axis('equal')
-        plt.savefig(save_filepath, dpi=300, bbox_inches='tight')
-        plt.close()
+# def plot_poses(data, output_save_path, timestamp):
+#     def plot_pose(pose, save_filepath):
+#         x, y = pose[:,0].numpy(), pose[:,1].numpy()
+#         plt.figure(figsize=(8,10))
+#         plt.scatter(x, y, c='red', s=50)
+#         for i,(xi, yi) in enumerate(zip(x, y)):
+#             plt.text(xi+0.02, yi+0.02, f"({xi:.2f},{yi:.2f})", fontsize=8, color='blue')
+#         skeleton = [
+#             (0,1),(1,2),(2,3),(3,4),(1,5),(5,6),(6,7),
+#             (1,8),(8,9),(9,10),(10,11),(8,12),(12,13),(13,14),
+#             (0,15),(15,17),(0,16),(16,18),(14,19),(19,20),(14,21),
+#             (11,22),(22,23),(11,24)
+#         ]
+#         for i,j in skeleton:
+#             plt.plot([x[i],x[j]], [y[i],y[j]], 'g-', linewidth=2)
+#         plt.gca().invert_yaxis()
+#         plt.axis('equal')
+#         plt.savefig(save_filepath, dpi=300, bbox_inches='tight')
+#         plt.close()
 
-    img_save_path = os.path.join(output_save_path, timestamp, "imgs")
-    shutil.rmtree(img_save_path)
-    os.makedirs(img_save_path, exist_ok=True)
-    poses = data.clone().cpu().reshape(-1, 25, 2)
-    poses = poses.clamp(min=-0.9999, max=0.9999)
-    poses = torch.atanh(poses)
-    idxs = torch.randperm(poses.shape[0])[:10]
-    for idx in idxs:
-        if torch.isfinite(poses[idx]).all():
-            plot_pose(poses[idx], os.path.join(img_save_path, f"{idx}.png"))
+#     img_save_path = os.path.join(output_save_path, timestamp, "imgs")
+#     shutil.rmtree(img_save_path)
+#     os.makedirs(img_save_path, exist_ok=True)
+#     poses = data.clone().cpu().reshape(-1, 25, 2)
+#     poses = poses.clamp(min=-0.9999, max=0.9999)
+#     poses = torch.atanh(poses)
+#     idxs = torch.randperm(poses.shape[0])[:10]
+#     for idx in idxs:
+#         if torch.isfinite(poses[idx]).all():
+#             plot_pose(poses[idx], os.path.join(img_save_path, f"{idx}.png"))
 
 
 def evaluate_loss_mpjpe(model, dataloader, criterion, need_normalize, timestamp, output_save_path):
     metric = Accumulator(8)
     device = next(iter(model.parameters())).device
     model.eval()
-    iterIdx = random.randint(0, len(dataloader))
+    # iterIdx = random.randint(0, len(dataloader))
     with torch.no_grad():
         for i, (x1, y1, z1, x2, y2, z2) in enumerate(dataloader):
             batch_size = x1.shape[0]
@@ -59,9 +59,7 @@ def evaluate_loss_mpjpe(model, dataloader, criterion, need_normalize, timestamp,
                 x1, x2 = normalize(x1), normalize(x2)
             x1, y1, z1, x2, y2, z2 = x1.to(device), y1.to(device), z1.to(device), x2.to(device), y2.to(device), z2.to(device)
             y1_hat, x2_hat, y2_hat = model(x1)
-
-            if i == iterIdx: plot_poses(y1_hat, output_save_path, timestamp)
-
+            # if i == iterIdx: plot_poses(y1_hat, output_save_path, timestamp)
             loss1, loss2, loss3 = criterion(y1_hat, y1), criterion(x2_hat, x2), criterion(y2_hat, y2)
             y1_hat, y1 = y1_hat.clamp(min=-0.9999, max=0.9999), y1.clamp(min=-0.9999, max=0.9999)
             y2_hat, y2 = y2_hat.clamp(min=-0.9999, max=0.9999), y2.clamp(min=-0.9999, max=0.9999)
@@ -70,9 +68,7 @@ def evaluate_loss_mpjpe(model, dataloader, criterion, need_normalize, timestamp,
             y1_hat, y1 = y1_hat * z1, y1 * z1
             y2_hat, y2 = y2_hat * z2, y2 * z2
             error1, error2 = torch.norm(y1_hat - y1, dim=-1).mean(), torch.norm(y2_hat - y2, dim=-1).mean()
-            
             metric.add(loss1.item() * batch_size, loss2.item() * batch_size, loss3.item() * batch_size, batch_size, error1.sum().item(), error1.numel(), error2.sum().item(), error2.numel())
-    
     return metric[0] / metric[3], metric[1] / metric[3], metric[2] / metric[3], metric[4] / metric[5], metric[6] / metric[7]
 
 
@@ -82,18 +78,14 @@ def train(model, train_loader, val_loader, loss_func, mask_ratio, lr, need_norma
             nn.init.xavier_uniform_(m.weight)
     model.apply(init_weights)
     model = nn.DataParallel(model, device_ids=devices).to(devices[0])
-
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     criterion = nn.MSELoss() if loss_func == "mse" else nn.L1Loss()
-
     min_val_mpjpe, best_epoch = float('inf'), 0
-
     for epoch in range(num_epochs):
         metric = Accumulator(8)
         model.train()
         for i, (x1, y1, z1, x2, y2, z2) in enumerate(train_loader):
             optimizer.zero_grad()
-
             batch_size = x1.shape[0]
             if need_normalize:
                 x1, x2 = normalize(x1), normalize(x2)
@@ -101,7 +93,6 @@ def train(model, train_loader, val_loader, loss_func, mask_ratio, lr, need_norma
             x1 = x1 * mask.float()
             x1, y1, z1, x2, y2, z2 = x1.to(devices[0]), y1.to(devices[0]), z1.to(devices[0]), x2.to(devices[0]), y2.to(devices[0]), z2.to(devices[0])
             y1_hat, x2_hat, y2_hat = model(x1)
-
             loss1, loss2, loss3 = criterion(y1_hat, y1), criterion(x2_hat, x2), criterion(y2_hat, y2)
             loss = alpha * loss1 + beta * loss2 + gamma * loss3
             loss.backward()
@@ -113,24 +104,19 @@ def train(model, train_loader, val_loader, loss_func, mask_ratio, lr, need_norma
             y1_hat, y1 = y1_hat * z1, y1 * z1
             y2_hat, y2 = y2_hat * z2, y2 * z2
             error1, error2 = torch.norm(y1_hat - y1, dim=-1).mean(), torch.norm(y2_hat - y2, dim=-1).mean()
-
             metric.add(loss1.item() * batch_size, loss2.item() * batch_size, loss3.item() * batch_size, batch_size, error1.sum().item(), error1.numel(), error2.sum().item(), error2.numel())
-
             if i != 0 and i % 20 == 0:
                 train_loss1, train_loss2, train_loss3, train_mpjpe1, train_mpjpe2 = metric[0] / metric[3], metric[1] / metric[3], metric[2] / metric[3], metric[4] / metric[5], metric[6] / metric[7]
                 print(f'Epoch: {epoch}, iter: {i}, train loss1: {train_loss1:.4f}, train loss2: {train_loss2:.4f}, train loss3: {train_loss3:.4f}, train mpjpe1: {train_mpjpe1:.4f}, train mpjpe2: {train_mpjpe2:.4f}')
-
         train_loss1, train_loss2, train_loss3, train_mpjpe1, train_mpjpe2 = metric[0] / metric[3], metric[1] / metric[3], metric[2] / metric[3], metric[4] / metric[5], metric[6] / metric[7]
         val_loss1, val_loss2, val_loss3, val_mpjpe1, val_mpjpe2 = evaluate_loss_mpjpe(model, val_loader, criterion, need_normalize, timestamp, output_save_path)
         logger.record([f'[{timestamp}] Epoch: {epoch}, train loss1: {train_loss1:.4f}, train loss2: {train_loss2:.4f}, train loss3: {train_loss3:.4f}, train mpjpe1: {train_mpjpe1:.4f}, train mpjpe2: {train_mpjpe2:.4f}'])
         logger.record([f'[{timestamp}] Epoch: {epoch},   val loss1: {val_loss1:.4f},   val loss2: {val_loss2:.4f},   val loss3: {val_loss3:.4f},   val mpjpe1: {val_mpjpe1:.4f},   val mpjpe2: {val_mpjpe2:.4f}'])
-
         if val_mpjpe1 + val_mpjpe2 < min_val_mpjpe:
             min_val_mpjpe = val_mpjpe1 + val_mpjpe2
             best_epoch = epoch
-            torch.save(model.state_dict(), os.path.join(output_save_path, timestamp, "checkpoints", f"epoch_{epoch}.pth"))
+            # torch.save(model.state_dict(), os.path.join(output_save_path, timestamp, "checkpoints", f"epoch_{epoch}.pth"))
         if epoch - best_epoch >= 20:
             break
-    
     logger.record([f'[{timestamp}] The best mpjpe occurred in epoch {best_epoch}'])
     return os.path.join(output_save_path, timestamp, "checkpoints", f"epoch_{epoch}.pth")

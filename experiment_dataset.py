bd3c6b476e16eb47bf4373a439e41d5aae60eeea
liming <xinglibao383@gmail.com>
2026-04-08 12:16:59 +0800

    logs

--- experiment_dataset.py
+++ experiment_dataset.py
@@ -65,10 +65,10 @@ def experiment_baseline(baseline):
 # nohup /home/yh/.conda/envs/myfuture/bin/python /mnt/mydata/yh/liming/workspace/future/experiment_dataset.py > /dev/null 2>&1 &
 # /home/yh/.conda/envs/myfuture/bin/python /mnt/mydata/yh/liming/workspace/future/experiment_dataset.py
 if __name__ == "__main__":
-    ours()
+    # ours()
     # experiment_baseline(baseline="pip_like_recon")
     # experiment_baseline(baseline="asip_like_recon")
     # experiment_baseline(baseline="mobileposer_like_recon")
-    # experiment_baseline(baseline="tip_like_recon")
+    experiment_baseline(baseline="tip_like_recon")
     # experiment_baseline(baseline="dynaip_like_recon")
     # experiment_baseline(baseline="imuposer_like_recon")
